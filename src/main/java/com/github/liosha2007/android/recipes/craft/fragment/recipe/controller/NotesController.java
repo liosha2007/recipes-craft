d25dab601688e40c9405f9fb2508741094d68ad8
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2014-10-05 04:02:05 +0300

    Updated database Added functional for edit item note

--- src/main/java/com/github/liosha2007/android/recipes/craft/fragment/recipe/controller/NotesController.java
+++ src/main/java/com/github/liosha2007/android/recipes/craft/fragment/recipe/controller/NotesController.java
@@ -1,9 +1,15 @@
 package com.github.liosha2007.android.recipes.craft.fragment.recipe.controller;
 
+import android.widget.Toast;
+
+import com.github.liosha2007.android.library.common.Utils;
 import com.github.liosha2007.android.library.fragment.controller.BaseFragmentController;
 import com.github.liosha2007.android.recipes.craft.database.DBHelper;
+import com.github.liosha2007.android.recipes.craft.database.dao.ItemDAO;
 import com.github.liosha2007.android.recipes.craft.database.domain.Item;
 import com.github.liosha2007.android.recipes.craft.fragment.recipe.view.NotesFragment;
+
+import java.sql.SQLException;
 
 /**
  * Created by liosha on 13.05.2014.
@@ -19,6 +25,25 @@ public class NotesController extends BaseFragmentController<NotesFragment> {
 
         int itemId = getArguments().getInt(com.github.liosha2007.android.recipes.craft.controller.RecipeController.ITEM_ID, -1);
         Item item = DBHelper.getItemDAO().queryForId(itemId);
-        view.showNote(item.getNote());
+        view.showNote(item.getNote(), itemId);
+    }
+
+    public void onEditClicked() {
+        view.switchToEdit();
+    }
+
+    public void onSaveClicked(String noteText, Integer itemId) {
+        if (itemId != null) {
+            ItemDAO itemDAO = DBHelper.getItemDAO();
+            Item item = itemDAO.queryForId(itemId);
+            item.setNote(noteText);
+            try {
+                itemDAO.update(item);
+                view.switchToView(noteText);
+            } catch (SQLException e) {
+                Utils.err("Can't save item note: " + e.getMessage());
+                Toast.makeText(getActivity(), "Ошибка при сохранении заметки: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- src/main/java/com/github/liosha2007/android/recipes/craft/fragment/recipe/view/NotesFragment.java
+++ src/main/java/com/github/liosha2007/android/recipes/craft/fragment/recipe/view/NotesFragment.java
@@ -1,5 +1,8 @@
 package com.github.liosha2007.android.recipes.craft.fragment.recipe.view;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.github.liosha2007.android.R;
@@ -17,9 +20,42 @@ public class NotesFragment extends BaseFragmentView<NotesController> {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        view(R.id.recipe_tab_note_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view(R.id.recipe_tab_note).getVisibility() == View.VISIBLE){
+                    controller.onEditClicked();
+                } else {
+                    controller.onSaveClicked(
+                            NotesFragment.this.<EditText>view(R.id.recipe_tab_note_edit).getText().toString(),
+                            (Integer) NotesFragment.this.view(R.id.recipe_tab_note).getTag()
+                    );
+                }
+            }
+        });
     }
 
-    public void showNote(String note) {
-        this.<TextView>view(R.id.recipe_tab_note).setText(note);
+    public void showNote(String note, int itemId) {
+        TextView noteView = this.view(R.id.recipe_tab_note);
+        noteView.setText(note);
+        noteView.setTag(itemId);
+    }
+
+    public void switchToEdit() {
+        TextView noteText = view(R.id.recipe_tab_note);
+        String noteString = noteText.getText().toString();
+        EditText noteEditor = view(R.id.recipe_tab_note_edit);
+        noteEditor.setText(noteString);
+        noteEditor.setVisibility(View.VISIBLE);
+        noteText.setVisibility(View.GONE);
+        this.<Button>view(R.id.recipe_tab_note_save).setText("Сохранить");
+    }
+
+    public void switchToView(String noteText) {
+        this.<TextView>view(R.id.recipe_tab_note).setText(noteText);
+        view(R.id.recipe_tab_note_edit).setVisibility(View.GONE);
+        view(R.id.recipe_tab_note).setVisibility(View.VISIBLE);
+        this.<Button>view(R.id.recipe_tab_note_save).setText("Редактировать");
     }
 }
